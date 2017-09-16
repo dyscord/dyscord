efdafa8d4af5bfb58475f6c9ab0db62596e98535
Lucien Gaitskell <lucieng@gaitskell.com>
2017-09-15 23:34:43 -0400

    Add 'PluginError' error superclass

--- dyscord/error.py
+++ dyscord/error.py
@@ -1,13 +1,18 @@
 # Plugin:
-class PluginNotFound(ValueError):
+class PluginError(Exception):
+    """ General plugin error """
+    pass
+
+
+class PluginNotFound(PluginError):
     """ Throw if plugin requested for download does not exist """
     pass
 
 
-class PluginExists(FileExistsError):
+class PluginExists(PluginError):
     """ Throw if plugin requested for download already exists """
 
 
-class PluginMalformedError(ValueError):
+class PluginMalformedError(PluginError):
     """ Throw if downloaded plugin is malformed """
     pass
