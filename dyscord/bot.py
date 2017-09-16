4b64650848954e35b18c16023ccd6f25a9e607ec
Lucien Gaitskell <lucieng@gaitskell.com>
2017-09-15 23:38:54 -0400

    Implement bot

--- dyscord/bot.py
+++ dyscord/bot.py
@@ -1,8 +1,62 @@
-from discord.ext.commands import Bot
+import discord
+from discord.ext.commands import Bot, command, Command
+from .plugin import ServerPluginHandler
+from .download import PluginManager
+from .error import PluginError, PluginAlreadyImported
+import os
+
+from typing import Dict
 
 # Bot version
 VERSION = 'v0.1'
 
+COMMAND_PREFIX = "d/"
+
 
 class Dyscord(Bot):
-    pass
+    def __init__(self):
+        super().__init__(COMMAND_PREFIX)
+
+        self.server_phandlers: Dict[discord.Guild, ServerPluginHandler] = {}
+        self.pm = PluginManager()
+
+        # Add all commands in class:
+        for m in dir(self):
+            attr = getattr(self, m)
+            if isinstance(attr, Command):
+                self.add_command(attr)
+
+    @command(pass_context=True)
+    async def plugin_install(self, ctx, plugin_name: str):
+        _guild = ctx.guild
+        _channel = ctx.channel
+
+        if _guild in self.server_phandlers:  # If the guild handler has been created
+            ph = self.server_phandlers[_guild]
+        else:  # Guild is new
+            ph = ServerPluginHandler(_guild.id)  # Create plugin handler
+            self.server_phandlers[_guild] = ph
+
+        try:
+            try:
+                ph.add_plugin(plugin_name, self.pm.get_plugin(plugin_name)(self))
+            except PluginAlreadyImported:
+                await _channel.send("Already implemented plugin: {}".format(plugin_name))
+            else:
+                await _channel.send("Successfully implemented plugin: {}".format(plugin_name))
+        except PluginError as e:
+            await _channel.send("Error implementing plugin: {}".format(e.__class__.__name__))
+
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        if message.author == self.user:  # Ignore if message is from this bot
+            return
+
+        await super().on_message(message)  # Run local commands
+        for ph in self.server_phandlers.values():
+            await ph.process_msg(message)
